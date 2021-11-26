d6a11eb6580008310bbc77139884d65a3d627e0e
Leoni Romain <leoniromain@hotmail.com>
2021-11-25 21:56:36 -0300

    feat: new class Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Board {
+	
+	private int rows;
+	private int column;
+	private Piece[][] pieces;
+	public Board(int rows, int column) {
+		this.rows = rows;
+		this.column = column;
+		pieces = new Piece[rows][column];
+	}
+	
+	
+
+}
